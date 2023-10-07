e18ca3f1415c8de4091c36b9d9712a9c9215b3f2
Roman Vaulin <vaulin007@yandex.ru>
2023-10-07 12:51:45 +0500

    pr14_1.4

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,11 @@
 const jwt = require('jsonwebtoken');
-
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError('Необходима авторизация');
+    return next(new UnauthorizedError('Необходима авторизация'));
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -15,8 +14,8 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'mesto');
   } catch (err) {
-    throw new UnauthorizedError('Необходима авторизация');
+    return next(new UnauthorizedError('Необходима авторизация'));
   }
   req.user = payload;
-  next();
+  return next();
 };
